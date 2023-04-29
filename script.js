b527ff53348ef30b9750120a034605f0e94e8a85
Alfredo Torres <alfredotorres.niu@gmail.com>
2023-04-28 22:44:52 -0600

    Funcional

--- script.js
+++ script.js
@@ -1,81 +1,45 @@
-$(document).ready(function() {
-    // Capturamos el formulario
-    var form = $('#myForm');
+var speed = 0;
+var prevSpeed = 0;
+var currentScale = 1;
 
-    // Capturamos el div donde se mostrará el mensaje de éxito
-    var alertSuccess = $('.alert-success');
-
-    // Capturamos el div donde se mostrará el mensaje de error
-    var alertError = $('.alert-danger');
-
-    // Ocultamos los mensajes de éxito y error
-    alertSuccess.hide();
-    alertError.hide();
-
-    // Validamos los campos del formulario
-    function validateForm() {
-        var isValid = true;
-
-        // Validamos el campo "Fecha"
-        var fecha = $('#fecha').val();
-        if (!fecha) {
-            $('#fechaError').html('La fecha es requerida');
-            isValid = false;
-        } else {
-            $('#fechaError').html('');
-        }
-
-        // Validamos el campo "Humedad"
-        var humedad = $('#humedad').val();
-        if (!humedad) {
-            $('#humedadError').html('La humedad es requerida');
-            isValid = false;
-        } else if (isNaN(humedad)) {
-            $('#humedadError').html('La humedad debe ser un número');
-            isValid = false;
-        } else {
-            $('#humedadError').html('');
-        }
-
-        // Validamos el campo "Temperatura"
-        var temperatura = $('#temperatura').val();
-        if (!temperatura) {
-            $('#temperaturaError').html('La temperatura es requerida');
-            isValid = false;
-        } else if (isNaN(temperatura)) {
-            $('#temperaturaError').html('La temperatura debe ser un número');
-            isValid = false;
-        } else {
-            $('#temperaturaError').html('');
-        }
-
-        return isValid;
+function increaseSpeed() {
+    if (speed < 180) {
+        speed = speed + 10;
+        addClass();
+        currentScale = currentScale + 1;
+        changeActive();
+        changeText();
     }
+}
 
-    // Enviamos el formulario mediante AJAX
-    form.submit(function(event) {
-        event.preventDefault();
+function decreaseSpeed() {
+    if (speed > 0) {
+        speed = speed - 10;
+        addClass();
+        changeActive();
+        currentScale = currentScale - 1;
+        changeText();
+    }
+}
 
-        if (!validateForm()) {
-            alertError.html('Por favor, corrija los errores en el formulario');
-            alertError.show();
-            alertSuccess.hide();
-        } else {
-            $.ajax({
-                type: form.attr('method'),
-                url: form.attr('action'),
-                data: form.serialize(),
-                success: function() {
-                    alertSuccess.html('Los datos se han enviado correctamente');
-                    alertSuccess.show();
-                    alertError.hide();
-                },
-                error: function() {
-                    alertError.html('Ha ocurrido un error al enviar los datos');
-                    alertError.show();
-                    alertSuccess.hide();
-                }
-            });
-        }
-    });
-});
+function addClass() {
+    let newClass = "speed-" + speed;
+    let prevClass = "speed-" + prevSpeed;
+    let el = document.getElementsByClassName("arrow-wrapper")[0];
+    if (el.classList.contains(prevClass)) {
+        el.classList.remove(prevClass);
+        el.classList.add(newClass);
+    }
+    prevSpeed = speed;
+}
+
+function changeActive() {
+    let tempClass = "speedometer-scale-" + currentScale;
+    let el = document.getElementsByClassName(tempClass)[0];
+    el.classList.toggle("active");
+}
+
+function changeText() {
+    let el = document.getElementsByClassName("km")[0];
+    el.innerText = speed;
+}
